c97d82e06cd71db54803813f3fcdf9a26db67161
The Tater Lord <111498793+TaterLord@users.noreply.github.com>
2023-05-31 21:08:27 +1000

    problems

--- assign2/header.inc.php
+++ assign2/header.inc.php
@@ -2,3 +2,12 @@
   <br><br>
   <h1><?php echo $pagetitle; ?></h1>
 </div>
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="topic.php">About ASP.NET</a></li>
+        <li><a href="quiz.php">Quiz</a></li>
+        <li><a href="manage.php">Manage</a></li>
+        <li style="float:right"><a href="enhancements.php">Enhancements</a></li>
+    </ul>
+</nav>
