c21996bf624e7a4d7b39106b5b601235bd10e3f9
Za1go <124943849+Za1go@users.noreply.github.com>
2023-05-31 18:18:54 +1000

    Update navigation.inc.php

--- assign2/navigation.inc.php
+++ assign2/navigation.inc.php
@@ -3,6 +3,7 @@
             <li><a href="index.php">Home</a></li>
             <li><a href="topic.php">About ASP.NET</a></li>
             <li><a href="quiz.php">Quiz</a></li>
+            <li><a href="manage.php">Manage</a></li>
             <li style="float:right"><a href="enhancements.php">Enhancements</a></li>
         </ul>
     </nav>
